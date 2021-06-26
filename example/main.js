fa9e2100ec08e73716599845c17db9f71b15cd91
Faris Han <farishanugraha@gmail.com>
2021-06-26 19:55:43 +0700

    Add npm script

--- example/main.js
+++ example/main.js
@@ -1,3 +1,13 @@
+console.log('hello from main.js')
+
+const $root = document.createElement('div')
+
+const player = new Player({
+  name: 'faris'
+})
+
+const items = []
+
 for (let index = 0; index < 10; index++) {
   const item = new Item({
     name: `item${index}`,
@@ -7,5 +17,12 @@ for (let index = 0; index < 10; index++) {
     }
   })
 
-  document.write(JSON.stringify(item))
+  items.push(item)
+
+  // $root.innerHTML += (JSON.stringify(item))
 }
+
+player.items = items
+$root.innerHTML += `<pre><code>${JSON.stringify(player, '', 2)}</code></pre>`
+
+document.body.appendChild($root)
